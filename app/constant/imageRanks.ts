8c6e255d88c86e69e5c7c01c41ff6c4cba6e9b0c
QueesQuees <queesca@gmail.com>
2022-10-02 17:01:17 +0700

    update image rank

--- app/constant/imageRanks.ts
+++
@@ -1,6 +0,0 @@
-export const imageRanks = {
-  rank1: require('../access/rankImage/rank1.jpg'),
-  rank2: require('../access/rankImage/rank2.jpg'),
-  rank3: require('../access/rankImage/rank3.jpg'),
-  rank4: require('../access/rankImage/rank4.jpg'),
-};
