5b98f645c5158db5e22d6f0a16bc8efc9e79731c
QueesQuees <queesca@gmail.com>
2022-10-02 17:11:18 +0700

    update image rank

---
+++ app/constant/imageRanks.ts
@@ -0,0 +1,6 @@
+export const imageRanks = {
+  rank1: require('../access/rankImage/bronze-flex-5v5.jpeg'),
+  rank2: require('../access/rankImage/gold-flex-5v5.jpeg'),
+  rank3: require('../access/rankImage/platinum-5v5-flex.jpeg'),
+  rank4: require('../access/rankImage/5v5-flex-diamond-summoner-icon.jpeg'),
+};
